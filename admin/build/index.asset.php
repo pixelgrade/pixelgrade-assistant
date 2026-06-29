78a0850acdc1b43e25bd9dd385607cadffb04ec7
George Olaru <george@pixelgrade.com>
2026-06-29 16:46:31 +0300

    Build Assistant admin bundle

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '1867b1dab4563598b182');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'd81e0a477cc44d90a074');
