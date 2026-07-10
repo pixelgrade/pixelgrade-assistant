2e2ae8282fb63465277889c9397a4fda6bfcbb1e
George Olaru <george@pixelgrade.com>
2026-07-10 15:33:15 +0300

    Build live design system previews

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'eb66bd3233244089765c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '4620e2a58d69aebc60e5');
